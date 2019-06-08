847b2901dda72e61af3e5dd4459ae8589ec248d8
Mateusz Duda <the_pro@wp.pl>
2019-06-08 19:11:54 +0200

    Added DHT22 and OneWire(WIP, needs address)

--- arduino/src/arduino.cpp
+++ arduino/src/arduino.cpp
@@ -1,9 +1,8 @@
 #include "Arduino.h"
+#include <DHT.h>
+#include <DS18B20.h>
+#include <OneWire.h>
 // the setup function runs once when you press reset or power the board
-void setup() {
-  // initialize digital pin LED_BUILTIN as an output.
-  Serial.begin(9600);
-}
 
 void unknown(char *command) {
   Serial.print("Unknown command: ");
@@ -14,7 +13,42 @@ void success(char mode, int pin, int state, char *buffer) {
   Serial.print("Buffer: ");
   Serial.println(buffer);
 }
-// the loop function runs over and over again forever
+
+void ds18b20(int pin) {
+  OneWire onewire(pin);
+  DS18B20 sensors(&onewire);
+  byte address[8] = {0x28, 0xC6, 0x71, 0xA9, 0x3, 0x0, 0x0, 0x4D};
+  sensors.begin();
+  sensors.request(address);
+  while (!sensors.available())
+    ;
+  float temperature = sensors.readTemperature(address);
+  Serial.print(temperature);
+  Serial.println(F(" 'C"));
+}
+
+void dht22(int pin) {
+  DHT dht(pin, DHT22);
+  dht.begin();
+  float h = dht.readHumidity();
+  float t = dht.readTemperature();
+
+  // check if returns are valid, if they are NaN (not a number) then something
+  // went wrong!
+  if (isnan(t) || isnan(h)) {
+    Serial.println("Failed to read from DHT");
+  } else {
+    Serial.print("Humidity: ");
+    Serial.print(h);
+    Serial.print(" %\t");
+    Serial.print("Temperature: ");
+    Serial.print(t);
+    Serial.println(" *C");
+  }
+}
+
+void setup() { Serial.begin(9600); }
+
 void loop() {
   int pin, state;
   char mode;
@@ -26,12 +60,24 @@ void loop() {
     case 2: {
       switch (mode) {
       case 'r': {
+        pinMode(pin, INPUT_PULLUP);
         Serial.println(digitalRead(pin));
         success(mode, pin, state, received);
         break;
       }
       case 'l': {
+        pinMode(pin, INPUT_PULLUP);
         Serial.println(analogRead(pin));
+        success(mode, pin, state, received);
+        break;
+      }
+      case '2': {
+        dht22(pin);
+        success(mode, pin, state, received);
+        break;
+      }
+      case '8': {
+        ds18b20(pin);
         success(mode, pin, state, received);
         break;
       }
@@ -44,17 +90,14 @@ void loop() {
     }
     case 3: {
       switch (mode) {
-      case 'p': {
-        pinMode(pin, state);
-        success(mode, pin, state, received);
-        break;
-      }
       case 'd': {
+        pinMode(pin, OUTPUT);
         digitalWrite(pin, state);
         success(mode, pin, state, received);
         break;
       }
       case 'a': {
+        pinMode(pin, OUTPUT);
         analogWrite(pin, state);
         success(mode, pin, state, received);
         break;
